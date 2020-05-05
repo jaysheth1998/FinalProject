c0b631ee57f8c75dcf8e0c5d56b8b360f757acec
alexhendel98 <46585579+alexhendel98@users.noreply.github.com>
2020-05-05 17:32:38 -0400

    added farmers

---
+++ Assets/Animations/AnimScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimScript : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public GameObject player;
+    private NavMeshAgent agent;
+    private Renderer renderer;
+    private int currentWaypoint;
+    private AudioSource audio;
+    private int timeSincePlayed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        renderer = GetComponent<Renderer>();
+        audio = GetComponent<AudioSource>();
+        currentWaypoint = 0;
+        timeSincePlayed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(waypoints[currentWaypoint].position, transform.position) < 3f) {
+            if (currentWaypoint == waypoints.Length - 1) {
+                currentWaypoint = 0;
+            } else {
+                currentWaypoint++;
+            }
+        }
+        agent.SetDestination(waypoints[currentWaypoint].position);
+        timeSincePlayed++;
+        if (Vector3.Distance(player.transform.position, transform.position) < 2f) {
+            if (timeSincePlayed > 200) {
+                audio.Play();
+                timeSincePlayed = 0;
+            }
+        }
+    }
+}
